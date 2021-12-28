7bb54883dad690688897311ae47bda7a55afbeb5
olyesung <olyesung@gmail.com>
2021-12-28 12:16:05 +0900

    remake

---
+++ navigators/LoggedInNav.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View } from "react-native";
+import TabIcon from "../components/nav/TabIcon";
+import SharedStackNav from "./SharedStackNav";
+
+const Tabs = createBottomTabNavigator();
+
+export default function LoggedInNav() {
+  return (
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Feed"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"home"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Feed" />}
+      </Tabs.Screen>
+      <Tabs.Screen
+        name="Search"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"search"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Search" />}
+      </Tabs.Screen>
+      <Tabs.Screen
+        name="Camera"
+        component={View}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"camera"} color={color} focused={focused} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notifications"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"heart"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Notifications" />}
+      </Tabs.Screen>
+      <Tabs.Screen
+        name="Me"
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <TabIcon iconName={"person"} color={color} focused={focused} />
+          ),
+        }}
+      >
+        {() => <SharedStackNav screenName="Me" />}
+      </Tabs.Screen>
+    </Tabs.Navigator>
+  );
+}
---
+++ screens/Feed.js
@@ -0,0 +1,45 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+import Photo from "../components/Photo";
+import ScreenLayout from "../components/ScreenLayout";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      ...PhotoFragment
+      user {
+        username
+        avatar
+      }
+      caption
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+    }
+  }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+`;
+
+export default function Feed() {
+  const { data, loading } = useQuery(FEED_QUERY);
+  console.log(data);
+  const renderPhoto = ({ item: photo }) => {
+    return <Photo {...photo} />;
+  };
+  return (
+    <ScreenLayout loading={loading}>
+      <FlatList
+        style={{ width: "100%" }}
+        showsVerticalScrollIndicator={false}
+        data={data?.seeFeed}
+        keyExtractor={(photo) => "" + photo.id}
+        renderItem={renderPhoto}
+      />
+    </ScreenLayout>
+  );
+}
